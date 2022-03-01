ccc1c6632a749425b8a165d27fcfb8a2893a8347
Aelish <alecrollison@icloud.com>
2022-03-01 15:17:52 -0500

    Add "  import "strings"  " and have a slice of ints representing the length of each string

--- go/shortestWord.go
+++ go/shortestWord.go
@@ -1,3 +1,5 @@
+import "strings"
+
 sampleSentence := "bitcoin take over the world IDK maybe"
 
 func find_short(sampleSentence):
@@ -5,4 +7,8 @@ func find_short(sampleSentence):
 
 	newSlice := strings.Split(sampleSentence, " ") //this splits the input string into words, each of which can be
 							//accessed using the the indices of the slice
+
+	for i := 0; i < len(newSlice); i++ {
+		result = append(result, len(newSlice[i]))
+	}
 
