5d363ce31bd56f4c49f79a57c7e83bd03a7481a3
Aelish <alecrollison@icloud.com>
2022-03-10 00:08:48 -0500

    Add the Sum of next n odd integers function

---
+++ go/computeCube.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+//returns the cube of the next n odd integers, starting from an input int. This
+//includes the starting number
+func SumOfNextNOddInts(startNumber, n int) int {
+	result := startNumber
+	for i := 1; i < n; i++ {
+		result += startNumber + 2*i
+		fmt.Println(result)
+	}
+	return result
+}
+
+
+func main() {
+	fmt.Println(SumOfNextNOddInts(5, 3))  //just a test
+}
+
+
