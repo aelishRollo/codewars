b2721c4598a8e816c538917fa11f1f180d2bc370
Aelish <alecrollison@icloud.com>
2022-03-24 13:06:09 -0400

    Rewrite ArrayIsOdd function. Account for negatives

--- go/parityCheckers.go
+++ go/parityCheckers.go
@@ -1,17 +1,35 @@
 package kata
 
+import "fmt"
+
 func ArrayIsEven(A []int) bool {
 	result := false
-	if (A[0]%2 == 0) && (A[1]%2 == 0) && (A[2]%2 == 0) {
+	numberOfEvenInts := 0
+
+	if A[0]%2 == 0 {
+		numberOfEvenInts++
+	}
+
+	if A[1]%2 == 0 {
+		numberOfEvenInts++
+	}
+
+	if A[2]%2 == 0 {
+		numberOfEvenInts++
+	}
+
+	if numberOfEvenInts >= 2 {
 		result = true
 	}
+
 	return result
 }
+
 
 func findOddInt(A []int) int {
 	result := 0
 	for i := 0; i < len(A); i++ {
-		if A[i]%2 == 1 {
+		if (A[i]%2 == 1) || A[i] % 2 == -1 {
 			result = A[i]
 		}
 	}
@@ -29,6 +47,7 @@ func findEvenInt(A []int) int {
 }
 
 func FindOutlier(integers []int) int {
+  fmt.Println(integers)
 	result := 0
 	if ArrayIsEven(integers) {
 		result = findOddInt(integers)
