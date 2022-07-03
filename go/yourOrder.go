88d066454c93301bedb70f17c47cfdbccce98612
Aelish <alecrollison@icloud.com>
2022-07-03 15:04:21 -0400

    Add the getNumFromString function.  Returns ints

--- go/yourOrder.go
+++ go/yourOrder.go
@@ -1,3 +1,10 @@
+import (
+	"fmt"
+	"strconv"
+)
+
+
+
 func isNumeric(a byte) bool {  // given a byte, returns a boolean representing whether it's a 0-9 number or not
 	result := false
 	if a == 48 || a == 49 || a == 50 || a == 49 || a == 51 || a == 52 || a == 53 || a == 54 || a == 55 || a == 56 {
@@ -6,4 +13,20 @@ func isNumeric(a byte) bool {  // given a byte, returns a boolean representing w
 
 	return result
 
+}
+
+
+
+func getNumFromString(a string) int { // given a string, returns the numbers within it as an int. Assumes only one set of numbers in a string
+	numbers := ""
+	for i := 0; i < len(a); i++ {
+		if isNumeric(a[i]) {
+			numbers += string(a[i])
+		}
+	}
+	result, err := strconv.Atoi(numbers)
+	if err != nil {
+		fmt.Println("Oh no, an error occured.")
+	}
+	return result
 }
