4bbc46ec1023d26bf00b7da7e8e71ba066961ed1
Aelish <alecrollison@icloud.com>
2022-07-12 01:33:16 -0400

    Comment basic ideas for approaching a solution

---
+++ javascript/rockPaperScissors.js
@@ -0,0 +1,17 @@
+const rps = (p1, p2) => {
+    let result = ""
+    let rock = ""
+    let paper = ""
+    let scissors = ""
+    let winner = ""
+    if (p1 == p2) {
+      result = "Draw!"
+    } else if ()
+    
+  };
+    
+    //analyze the total results
+    //based on the results, assign "winner" to the winner 
+    //For example, rock, paper,so we know paper is the winner.
+    //Now we search for which one is paper, and assign it winner
+    //maybe by having 3 variables called rock paper, and scissors
