d531957a7089713cfafa678f3fa673fcac391c34
Aelish <alecrollison@icloud.com>
2022-07-14 17:57:25 -0400

    write the function, small semantic error to fix

---
+++ go/josephus.go
@@ -0,0 +1,45 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func deleteRuneFromString(s string, n int) string { //returns the same string without the nth rune
+	result := ""
+	for i := 0; i < len(s); i++ {
+		if i != n {
+			result += string(s[i])
+		}
+	}
+	return result
+}
+
+//number keeps being (3 +3i) % length(array)
+
+func JosephusSurvivor(n, k int) int {
+	var result int
+	numbers := ""
+	for i := 1; i < n+1; i++ {
+		numbers += strconv.Itoa(i)
+	} //This bit makes the array of starting numbers, representing each person
+	fmt.Println(numbers)
+	for i := 0; i < n; i++ {
+		fmt.Println("the thing being deleted is")
+		fmt.Println((2 + 2*i) % len(numbers))
+		numbers = deleteRuneFromString(numbers, (2+2*i)%len(numbers))
+		fmt.Println(numbers)
+
+	}
+
+	fmt.Println(numbers)
+	return result
+}
+
+func main() {
+	JosephusSurvivor(7, 3)
+	fmt.Println("Hello, 世界")
+	fmt.Println(deleteRuneFromString("sunday", 3))
+}
