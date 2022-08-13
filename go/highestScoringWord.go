d5d6a0673215aadffdf0641c8161a2db92382836
Aelish <alecrollison@icloud.com>
2022-08-12 20:00:42 -0400

    Nearly finish High(string) string function Just need to get the highest scoring number out of the dictionary and return its value

--- go/highestScoringWord.go
+++ go/highestScoringWord.go
@@ -1,16 +1,40 @@
-func scoreFromWord(word string) int {  		//returns the score of a string, as an int. Score being the sum of letter scores, where a == 1, b == 2, etc
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func scoreFromWord(word string) int { //returns the score of a string, as an int. Score being the sum of letter scores, where a == 1, b == 2, etc
 	result := 0
 	for i := 0; i < len(word); i++ {
-		fmt.Println(word[i] - 96)
 		result += int(word[i] - 96)
 	}
 	return result
 }
 
 
+func High(s string) string {
+    
 
-
-
+	return "pleura necromancer"
+}
 
 //for each word in the array, create a dictionary entry associating its text with its score
 //then iterate through and find the highest score, returning the associated string
+
+func main() {
+
+	m := make(map[string]int) //make the dictionary with string typed keys and int types values
+
+	str := "looking through your eyes from a distance fit tightly like a dovetail"
+	stringArray := strings.Fields(str)
+	for i := 0; i < len(stringArray); i++ {
+		m[stringArray[i]] = scoreFromWord(stringArray[i])
+	}
+
+	fmt.Println(m)
+}
+
