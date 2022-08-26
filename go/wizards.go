926cd7b448361579bdab498712b7874badd2cb93
Aelish <alecrollison@icloud.com>
2022-08-25 20:57:16 -0400

    Write bodies of left/right letter & adjacenteWiz.

--- go/wizards.go
+++ go/wizards.go
@@ -32,16 +32,58 @@ func power(str string) int { // Returns positive numbers for left side, negative
 	return result
 }
 
-func isRightLetter(str string) bool {}
+func isRightLetter(str string) bool {
+	result := false
+	if (str == "m") || (str == "q") || (str == "d") || (str == "z") || (str == "j") {		//returns whether the letter is a letter in the Right group
+		result = true
+	}
+	return result
 
-func isLeftLetter(str string) bool {}
+}
 
-func adjacentWizards(str, i) string {
+
+func isLeftLetter(str string) bool {			//returns whether the letter is a letter in the Left group
+	result := false
+	if (str == "w") || (str == "p") || (str == "b") || (str == "s") || (str == "t") {
+		result = true
+	}
+	return result
+}
+
+func adjacentWizards(str string, i int) string {
 	//i is which character of the string you're checking.
-	//return either "left", "right", or "nothing" to signify which wizard is present
+	//return either "left", "right", or "none" to signify which wizard is present
+	result := "none"
+	var left bool
+	var right bool
+
+	//i is which character of the string you're checking.
+	//return either "left", "right", or "none" to signify which wizard is present
+
+	if (string(str[i-1]) == "t") || (string(str[i+1]) == "t") { //check if either the previous or next entry is the left wizard
+		left = true
+	}
+	if (string(str[i-1]) == "j") || (string(str[i+1]) == "j") { //check if either the previous or next entry is the right wizard
+		right = true
+	}
+
+	if left {
+		result = "left"
+	}
+	if right {
+		result = "right"
+	}
+	if left && right {
+		result = "none"
+	}
+	if !(left || right) {
+		result = "none"
+	}
+	return result
 } 
 
-//iterate through each char in the string. Maintain a total, adding each member to the result 
+//iterate through each char in the string. Maintain a total, adding each member to the result. Otherwise
+//Now that wizards are able to be taken care of (edge case in case the second rune is a wizard, need to take care of that), we can write this loop which sums the powers to find the victors.
 
 func main() {
 
