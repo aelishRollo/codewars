81d04a2b7d887563a11dfbcea8f877347f6ca783
Aelish <alecrollison@icloud.com>
2022-09-16 21:13:12 -0400

    Export some basic types

---
+++ typescript/basictypes.ts
@@ -0,0 +1,13 @@
+export var var1Boolean: boolean = true;
+export var var2Decimal: decimal = 13;
+export var var3Hex: hex = f00d;
+export var var4Binary: binary = 111111;
+export var var5Octal: octal = 744;
+
+export var var6String:  string = "Hello, World!";
+
+export var var7Array:  array = [1, 'test', {a:3}, 4, 5];
+
+export var var8NumericArray:  numericArray = [1,2,3,4,5];
+
+export var var9Tuple:  = [];
