8d12a9cc0eca03fa4077210c44d948b015b8ae36
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-01 12:45:54 -0400

    Complete twiceLinear.js

---
+++ twiceLinear.js
@@ -0,0 +1,96 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        this._bubbleUp();
+    }
+
+    pop() {
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._sinkDown();
+        return min;
+    }
+
+    _bubbleUp() {
+        let idx = this.heap.length - 1;
+        const element = this.heap[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.heap[parentIdx];
+            if (element >= parent) break;
+            this.heap[idx] = parent;
+            this.heap[parentIdx] = element;
+            idx = parentIdx;
+        }
+    }
+
+    _sinkDown() {
+        let idx = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.heap[leftChildIdx];
+                if (leftChild < element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.heap[rightChildIdx];
+                if (
+                    (swap === null && rightChild < element) ||
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.heap[idx] = this.heap[swap];
+            this.heap[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
+function dblLinear(n) {
+    const heap = new MinHeap();
+    const seen = new Set([1]);
+    heap.push(1);
+
+    let val = 1;
+
+    for (let i = 0; i <= n; i++) {
+        val = heap.pop();
+
+        let y = 2 * val + 1;
+        let z = 3 * val + 1;
+
+        if (!seen.has(y)) {
+            seen.add(y);
+            heap.push(y);
+        }
+
+        if (!seen.has(z)) {
+            seen.add(z);
+            heap.push(z);
+        }
+    }
+
+    return val;
+}
+
+console.log(dblLinear(10)); // Should output 22
