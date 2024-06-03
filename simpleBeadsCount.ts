822e88ad5f2f4ab6f081e7ba4e4f8d845cc7a9a8
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-02 22:16:02 -0400

    write simpleBeadsCount.ts

---
+++ simpleBeadsCount.ts
@@ -0,0 +1,10 @@
+export function countRedBeads(n: number): number {
+    let result:number
+    if (n <= 0) {
+      result = 0
+    } else {
+      return (n-1)*2  
+    }
+    return result
+  }
+  
