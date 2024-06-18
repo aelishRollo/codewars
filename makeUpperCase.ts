43cb82710834a8ddfbc131d53923b7fbf0327ba4
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-18 12:47:41 -0400

    Write makeUpperCase.ts

---
+++ makeUpperCase.ts
@@ -0,0 +1,14 @@
+export function makeUpperCase(str: string): string{
+    return str.toUpperCase();
+  }
+  
+  
+  //PREP
+  
+  //Parameters: a string
+  
+  //Return value: an uppercase verson of the string
+  
+  //Example: 'bonnaroo' => 'BONNAROO'
+  
+  //Psuedocode: return str.toUpperCase()
