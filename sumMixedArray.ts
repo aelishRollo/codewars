ecc5e3a2df8eac2a823214debe15ebdf881e72b5
aelishRollo <alecrollison@icloud.com>
2024-06-27 12:31:36 +0000

    Auto-update sumMixedArray.ts

--- sumMixedArray.ts
+++ sumMixedArray.ts
@@ -2,3 +2,4 @@ export function sumMix(x: any[]): number {
     return x.reduce((accum,currentNum) => accum + Number(currentNum),0) 
   }
 console.log('Auto-update for sumMixedArray.ts');
+console.log('Auto-update for sumMixedArray.ts');
