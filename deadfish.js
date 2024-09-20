c757a47567743db0307fc6367ccd9df576321cc2
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-09-20 15:41:05 -0400

    Code deadFish.js

--- deadfish.js
+++ deadfish.js
@@ -1,20 +1,46 @@
 // Return the output array, and ignore all non-op characters
-function parse(data) {
-    let currentValue = 0
-    let result = []
-    for (let letter of data) {
-      if (letter === 'i') {
-        currentValue++
-      } else if (letter === 'd') {
-        currentValue--
-      } else if (letter === 's') {
-        currentValue = currentValue * currentValue
-      } else if (letter === 'o') {
-        result = [...result,currentValue]
-      }
+function parse( data ) {
+  deadFish.n = 0;
+  deadFish.result = [];
+  
+  for (let letter of data) {
+    if (deadFish[letter]) {
+      deadFish[letter]()
     }
-    return result
   }
-  
-  
-  
+  return deadFish.result
+}
+
+const deadFish = {
+  n: 0,
+  result: [],
+  i: () => deadFish.n++,
+  d: () => deadFish.n--,
+  s: () => deadFish.n = deadFish.n * deadFish.n,
+  o: () => deadFish.result.push(deadFish.n)
+};
+
+/*
+Parameters: 
+a string, where each character represents an instruction
+
+Return Value:
+an array, consisting of values computed by the deadfish
+
+Example:
+parse("iiisdoso")
+increments i up to 3,
+squares i, making it 9,
+decrements i to 8,
+outputs i to the array
+squares i to 64
+outputs it to the array
+
+Psuedocode:
+just have make a deadfish object, with:
+an i field, 
+a result field,
+an increment / decrement method, 
+and an output to array method
+
+*/
